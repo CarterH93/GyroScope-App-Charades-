383adbd03bd93c16dfa69956efb7c7ea56fca934
CarterH93 <Carterhawkins93@gmail.com>
2022-09-25 23:56:14 -0400

    Code added

--- GyroScope App (Charades)/ContentView.swift
+++ GyroScope App (Charades)/ContentView.swift
@@ -1,21 +1,45 @@
 //
 //  ContentView.swift
-//  GyroScope App (Charades)
+//  test2
 //
-//  Created by Carter Hawkins on 9/25/22.
+//  Created by Carter Hawkins on 9/24/22.
 //
 
 import SwiftUI
+import CoreMotion
+
+let motionManager = CMMotionManager()
+
 
 struct ContentView: View {
+    @State private var colorChange = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Circle()
+                .foregroundColor(colorChange ? .green : .blue)
+                .frame(width: 200, height: 200)
         }
         .padding()
+        .onAppear() {
+            motionManager.gyroUpdateInterval = 0.1
+            
+            motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+                if let myData = data {
+                    if myData.rotationRate.y > 8 {
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            colorChange = true
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500), execute: {
+                                
+                                withAnimation(.easeInOut(duration: 0.5)) {
+                                    colorChange = false
+                                }
+                            })
+                        }
+                    
+                }
+            }
+        }
     }
 }
 
